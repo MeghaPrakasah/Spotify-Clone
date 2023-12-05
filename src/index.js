f9dabadc2ee6e2f2d121e0b1b9ad8efe6c9317d9
MeghaPrakasah <meghatp12345@gmail.com>
2023-12-05 20:02:49 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { StateProvider } from './utils/StateProvider';
+import reducer,{initialState} from './utils/Reducer';
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+
+ReactDOM.render(
+  <React.StrictMode>
+    <StateProvider initialState={initialState} reducer={reducer}>
+    <App />
+    </StateProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
